bb39e1f5df1f8d19a911a5b0d3be85857f5ba3de
visitmyaccount <visitmyaccount@gmail.com>
2017-03-15 15:19:31 +0530

    Testing application

--- JavaApplication21-2/src/javaapplication21/pkg2/JavaApplication212.java
+++ JavaApplication21-2/src/javaapplication21/pkg2/JavaApplication212.java
@@ -17,6 +17,7 @@ public class JavaApplication212 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Testing one");
+        System.out.println("More testing");
     }
     
 }
